f13046284055b616ee2695c5c98de66b807948e2
AditiJain1001 <codingwizard101@gmail.com>
2023-08-18 09:10:42 -0400

    adding some helpful comments for later funtionality

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// how to add keyborad funtionality by pressing enter on keyboard
+// how to clear the value in search bar after save input clicked and or enter pressed 
+// get chatgpt to orangize code and add appripriate comments
+// ask chatgpt if there is additional funtionality that would be cool to add
+
 let myLeads = [];
 
 //input element
